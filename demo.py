30e7157172fd18674e757f8336e8a693e15033a6
Joshua Reid <60335156+joshuarreid@users.noreply.github.com>
2022-04-18 14:52:05 -0400

    demo.py - josh

--- demo.py
+++ demo.py
@@ -16,9 +16,44 @@ class Todo(db.Model):
    content = db.Column(db.String, nullable=False)
    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello"
+   if request.method == 'POST':
+       company = request.form['content']
+       googlenews.search(company)
+       results = googlenews.results()
+       res = []
+       for result in results:
+           temp = []
+           temp.append("TITLE: " + result["title"])
+           temp.append("DESC: " + result["desc"])
+           temp.append("URL: " + result["link"])
+           res.append('\n'.join(temp))
+       res.insert(0,company.upper())
+       body = '\n\n'.join(res)
+       new_task = Todo(content=body)
+       googlenews.clear()
+       try:
+           db.session.add(new_task)
+           db.session.commit()
+           return redirect('/')
+       except:
+           return 'There was an issue adding your task'
+   else:
+       tasks = Todo.query.order_by(Todo.date_created).all()
+       return render_template('index.html', tasks=tasks)
+ 
+ 
+@app.route('/delete/<int:id>')
+def delete(id):
+   task_to_delete = Todo.query.get_or_404(id)
+ 
+   try:
+       db.session.delete(task_to_delete)
+       db.session.commit()
+       return redirect('/')
+   except:
+       return 'There was a problem deleting that task'
 
 
 if __name__ == "__main__":
