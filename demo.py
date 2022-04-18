c8c115db3c2eb296c9324fbc3bb831cc7360d037
Manas Angalakuduti <55326670+manasangalakuduti@users.noreply.github.com>
2022-04-18 14:50:27 -0400

    updating demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,16 @@ from sqlalchemy.dialects.mysql import LONGTEXT
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+googlenews = GoogleNews(lang='en', period='`5d')
+ 
+class Todo(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   content = db.Column(db.String, nullable=False)
+   date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 @app.route('/')
 def index():
     return "Hello"
